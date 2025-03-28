c5c9008cff37b54e4e6c69491fde2c2cf47dbe70
Daniel <dan.g.g@hotmail.es>
2025-03-28 04:54:18 -0600

    Creacion de repositorio, creación de proyecto, vista: Layout,Index,ContacUs,Abogados, GeneracionDocsAutomatizados

---
+++ Praecepta.UI/Controllers/DocsGeneratorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Praecepta.UI.Controllers
+{
+    public class DocsGeneratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DocsGenerator()
+        {
+            return View();
+        }
+    }
+}
---
+++ Praecepta.UI/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using Praecepta.UI.Models;
+using System.Diagnostics;
+
+namespace Praecepta.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About() 
+        {
+            return View();
+        }
+
+        public IActionResult Attorneys()
+        {
+            return View();
+        }
+
+        public IActionResult Testimonials()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        
+        public IActionResult PracticeCrimeLaw()
+        {
+            return View("Practice/PracticeCrimeLaw");
+        }
+
+        public IActionResult AttorneyDetails()
+        {
+            return View("AttorneyDetails/AttorneyDetails");
+        }
+
+    
+
+
+
+    }
+}
