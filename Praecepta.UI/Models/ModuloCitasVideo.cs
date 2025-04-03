b4f14a3998838b8af898cb1887e21772a50bf9b1
DanGonzaG <dan.g.g@hotmail.es>
2025-04-02 20:08:57 -0600

    Tanto este como los otros commits con numero corresponden a cambios en vistas y logica

---
+++ Praecepta.UI/Models/ModuloCitasVideo.cs
@@ -0,0 +1,14 @@
+﻿using NuGet.Protocol.Core.Types;
+
+namespace Praecepta.UI.Models
+{
+    public class ModuloCitasVideo
+    {
+        public int NumCita { get; set; }
+        public string Evento { get; set; }
+        public DateTime FechaHora { get; set; }
+        
+
+       
+    }
+}
---
+++ Praecepta.UI/wwwroot/lyso/js/Daniel.js
@@ -0,0 +1,48 @@
+﻿document.getElementById('PrevioBtn').addEventListener('click', () => {
+    const TipoCaso = document.getElementById('TipoCaso').value;
+    const titulo = document.getElementById('titulo').value;
+    const Pruebas = document.getElementById('Pruebas').value;
+    const NomCliente = document.getElementById('NomCliente').value;
+    const descripcionCaso = document.getElementById('descripcionCaso').value;
+    
+
+    // Actualiza el contenido del modal
+    document.getElementById('previoTipoCaso').textContent = TipoCaso;
+    document.getElementById('previotitulo').textContent = titulo;
+    document.getElementById('previoPruebas').textContent = Pruebas;
+    document.getElementById('previoCliente').textContent = NomCliente;
+    document.getElementById('previoDescripcion').textContent = descripcionCaso;
+
+    // Muestra el modal usando Bootstrap
+    const previewModal = new bootstrap.Modal(document.getElementById('PrevioModal'));
+    previewModal.show();
+
+
+    document.getElementById('Retroceso').addEventListener('click', () => {
+       
+        document.getElementById('TipoCaso').value = TipoCaso;
+        document.getElementById('titulo').value = titulo;
+        document.getElementById('Pruebas').value = Pruebas;
+        document.getElementById('NomCliente').value = NomCliente;
+        document.getElementById('descripcionCaso').value = descripcionCaso;
+        const previewModalInstance = bootstrap.Modal.getInstance(document.getElementById('PrevioModal'));
+        if (previewModalInstance) {
+            previewModalInstance.hide(); // Cerrar modal correctamente
+        };
+
+        const modalBackdrops = document.querySelectorAll('.modal-backdrop');
+        modalBackdrops.forEach((backdrop) => {
+            backdrop.parentNode.removeChild(backdrop); // Quitar el fondo
+        });
+
+        // Reactiva el scroll de la página
+        document.body.style.overflow = 'auto';
+        document.documentElement.style.overflow = 'auto';
+        document.body.style.position = 'static'; // Asegura que el body pueda desp
+
+
+
+        console.log('Cerrando el modal y eliminando el fondo...');
+
+    });
+});
